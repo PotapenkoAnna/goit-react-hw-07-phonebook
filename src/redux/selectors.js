b4ec8c8a6fba1abada21f6f3deb347b0aea2d927
Potapenko Anna <potapka23@gmail.com>
2023-11-25 19:06:18 +0200

    commit

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,5 +1,18 @@
-//import { createSelector } from '@reduxjs/toolkit';
+import { createSelector } from '@reduxjs/toolkit';
 
-export const selectContacts = state => state.contacts.contacts; 
-export const selectFilter = state => state.filter;
+export const selectContacts = state => state.contacts.items;
+
+export const selectIsLoading = state => state.contacts.isLoading;
+export const selectError = state => state.contacts.error;
+export const selectFilter = state => state.filter.select;
+
+export const selectVisibleContacts = createSelector( 
+  [selectContacts, selectFilter],
+  (items, select) => {
+    if (select !== null) {
+      return items.filter(item => item.name.toLowerCase().includes(select));
+    }
+    return items;
+  }
+); 
 
